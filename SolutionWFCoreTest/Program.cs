7b23810d2e00b84f5fb64700033b4540cacb91f6
Serguei Anikanov <SAnikanov@nvg.ru>
2021-11-02 17:25:46 +0300

    Initial commit

---
+++ SolutionWFCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolutionWFCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
